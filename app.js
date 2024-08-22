32e02a07e1b4c1e2bc4417c1ab6af2cef491d5ec
moisterchief <86637326+moisterchief@users.noreply.github.com>
2024-08-22 23:35:25 +1200

    Update app.js
    remove comments

--- app.js
+++ app.js
@@ -14,14 +14,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 
 //define routes for handling different HTTP methods and paths
-//CRUD operations
-// app.post('/events', events.createEvent); //create a new event 
-// app.get('/events', events.getAllEvents); //get all events
-// app.get('/events/:id', events.getEventById); //get an event by its ID
-// app.get('/events/name/:name', events.getEventByName); //get an event by its name
-// app.put('/events/:id', events.updateEventById); //update an event by its ID
-// app.delete('/events/:id', events.deleteEventById); //delete an event by its ID
-// app.delete('/events', events.deleteAllEvents); //Delete all events
 app.post('/slack/action-endpoint', events.slackEndpoint); 
 
 //start the express server and listen for incoming connections
